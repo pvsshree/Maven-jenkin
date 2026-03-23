ba438396cd826d90c64a7e9e2955d3bd9de0623d
PayalS <pvsshreegajanan@gmail.com>
2026-03-23 21:49:57 +0530

    parameter added

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -2,20 +2,35 @@ package mavenforjenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UITest {
 
+	@Parameters("Browser")
 	@Test
-	public void startBrowser()
+	public void startBrowser(String browserName)
 	
 	{
+		System.out.println("Parameter name is" +browserName);
 		
+		WebDriver driver = null;
+		if (browserName.contains("chrome"))
+		{
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		 driver = new ChromeDriver();
+		}
+	
+		 else if(browserName.contains("edge"))
+		 {
+			 WebDriverManager.edgedriver().setup();
+			 driver = new EdgeDriver();
+		 }
+		 //	WebDriver driver = new ChromeDriver();
 		driver.manage().window().fullscreen();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
